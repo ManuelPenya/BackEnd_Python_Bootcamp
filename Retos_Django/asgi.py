db888f1dd306b3ed15dcd625b4771cdb9a8b69e1
Manuel <manuel@somdata.es>
2021-06-29 09:02:31 +0200

    Reto Hero API rest django DONE

---
+++ Retos_Django/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Retos_Django project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Retos_Django.settings')
+
+application = get_asgi_application()
